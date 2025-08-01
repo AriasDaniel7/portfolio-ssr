4c9d2be629095af216e823cbef0b732384e03db3
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-08-01 17:27:07 -0500

    loading home

--- src/app/portfolio/layouts/portfolio-layout/portfolio-layout.component.ts
+++ src/app/portfolio/layouts/portfolio-layout/portfolio-layout.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PortfolioNavComponent } from '@portfolio/components/portfolio-nav/portfolio-nav.component';
+import { TextLoadingComponent } from '@shared/components/text-loading/text-loading.component';
 
 @Component({
   selector: 'app-portfolio-layout',
-  imports: [RouterOutlet, PortfolioNavComponent],
+  imports: [RouterOutlet, PortfolioNavComponent, TextLoadingComponent],
   templateUrl: './portfolio-layout.component.html',
   styleUrl: './portfolio-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
